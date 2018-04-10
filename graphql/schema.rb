da547a6dbe5ac9fc8c7ccf292dd58c4a7d1ad392
sebpearce <sebpearce@gmail.com>
2018-04-10 23:22:46 +1000

    Add transactions query

--- graphql/schema.rb
+++ graphql/schema.rb
@@ -4,12 +4,26 @@ require_relative '../lib/cycad'
 
 QueryType = GraphQL::ObjectType.define do
   name 'Query'
+  description 'the query root'
 
   field :transaction do
     type TransactionType
     argument :id, !types.ID
     description 'Find a transaction by ID'
-    resolve ->(obj, args, ctx) { Cycad.find_transaction(args[:id]) }
+    resolve -> (obj, args, ctx) { Cycad.find_transaction(args[:id]) }
+  end
+
+  field :category do
+    type CategoryType
+    argument :id, !types.ID
+    description 'Find a category by ID'
+    resolve -> (obj, args, ctx) { Cycad.find_category(args[:id]) }
+  end
+
+  field :transactions, types[TransactionType] do
+    resolve -> (obj, args, ctx) {
+      Cycad.transactions
+    }
   end
 end
 
